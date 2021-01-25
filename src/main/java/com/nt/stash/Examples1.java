4d5f3c04fa97cd9c26e143f66ba18470a1876725
Rakesh <kusumarakesh235@gmail.com>
2021-01-25 14:59:55 +0530

    added to localrepositores

---
+++ src/main/java/com/nt/stash/Examples1.java
@@ -0,0 +1,8 @@
+package com.nt.stash;
+
+public class Examples1 {
+	public void use() {
+		System.out.println("status");
+	}
+
+}
---
+++ src/test/java/com/example/rakesh/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rakesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
